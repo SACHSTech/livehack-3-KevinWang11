cabee517ca3819f05e9de9559e737c3d03b41202
KevinWang1122 <yukai.wang23@ycdsbk12.ca>
2021-03-03 15:00:49 +0000

    Finished code

--- problem1.py
+++ problem1.py
@@ -1,1 +1,30 @@
+"""
+-------------------------------------------------------------------------------
+Name:   filename.py
+Purpose:  a description of your program
+ 
+Author: lastname.First Initial
+ 
+Created:  date in dd/mm/yyyy
+------------------------------------------------------------------------------
+"""
+print("****** Tournament Tracker ******")
+print ("")
+win_count = 0 
+loss_count = 0
 
+for i in range(6):
+  result = input("Enter the wins and losses for your team (W/L): ")
+  if result == 'W':
+    win_count += 1
+  elif result == 'L':
+    loss_count += 1
+
+if win_count == 5 or win_count == 6:
+  print("You have been placed in group 1.")
+elif win_count == 3 or win_count == 4:
+  print("You have been placed in group 2.")
+elif win_count == 1 or win_count == 2:
+  print ("You have been placed in group 3.")
+else:
+  print ("You have been eliminated from the tournament.")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,25 @@
+"""
+-------------------------------------------------------------------------------
+Name:   filename.py
+Purpose:  a description of your program
+ 
+Author: lastname.First Initial
+ 
+Created:  date in dd/mm/yyyy
+------------------------------------------------------------------------------
+"""
+print("***** Welcome to the DoorDash Distance Tracker *****")
+print("")
+distance = 0
+count = 0
+
+while distance < 100:
+  distance_traveled = int(input("Enter the distance travelled for the day: "))  
+  distance = distance + distance_traveled
+  count += 1
+    
+average = distance/count
+
+print ("It took", count, "days to surpass 100km driven.")
+print("The average distance driven per day is",int(average), "km")
 
